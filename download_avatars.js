252a06a12f4d345f3a801cd7931c83e19a823ad3
Kenzie Bullock <kenzieb_13@shaw.ca>
2018-09-03 22:56:52 +0000

    updated download_avatars to print out avatar urls

--- download_avatars.js
+++ download_avatars.js
@@ -5,20 +5,29 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 // function to get contributors
 function getRepoContributors(repoOwner, repoName, cb) {
+    
     const options = {
         url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
         headers: {
             'User-Agent': 'request',
-            'Authorization': '' 
+            'Authorization': secrets.GITHUB_TOKEN
         }
     };
     
     request(options, function(err, res, body) {
-        cb(err, body);
+        
+        if (err) {
+            console.log(err);
+        }
+        if (body) {
+            cb(err, JSON.parse(body))
+        } 
     });
 }
 
 getRepoContributors('jquery', 'jquery', function(err, result) {
-    console.log('Errors:', err);
-    console.log('Results', result);
+    //console.log('Errors:', err);
+    for ( var key in result) {
+        console.log(result[key].avatar_url);
+    }
 });
