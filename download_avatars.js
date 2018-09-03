3159789cba161dd09136294cafb6b2f86a25482b
Kenzie Bullock <kenzieb_13@shaw.ca>
2018-09-03 20:53:37 +0000

    added personal access token

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,21 @@
-var request = require('request');
+const request = require('request');
+const secrets = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 // function to get contributors
 function getRepoContributors(repoOwner, repoName, cb) {
-    //  
+    const options = {
+        url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+        headers: {
+            'User-Agent': 'request',
+            'Authorization': '' 
+        }
+    };
+    
+    request(options, function(err, res, body) {
+        cb(err, body);
+    });
 }
 
 getRepoContributors('jquery', 'jquery', function(err, result) {
